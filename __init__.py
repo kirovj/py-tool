057ad6225b181379a0481d960f3e20259d43bb90
Jeremiah <31985312+J-wyt@users.noreply.github.com>
2018-08-03 09:59:44 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,11 @@
 # encoding: utf-8
 '''
 @author: Jeremiah
-@contact: wuyiting@myhexin.com
 @file: __init__.py
 @time: 2018/6/26 13:43
 @desc:
 '''
 
-# import GUI
-#
-# GUI.main()
+import GUI
+
+GUI.main()
