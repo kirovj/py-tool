7adeb9e6e461f8f93f41a67651eeb0a4fedb1ff7
Jeremiah <31985312+J-wyt@users.noreply.github.com>
2018-08-03 10:04:58 +0800

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -1,7 +1,6 @@
 # encoding: utf-8
 '''
 @author: Jeremiah
-@contact: wuyiting@myhexin.com
 @file: Utils.py
 @time: 2018/6/28 13:57
 @desc:
@@ -20,19 +19,6 @@ def urlEncode(string):
 
 def urlDecode(string):
     return unquote(string)
-
-def congratulations():
-    week = str(datetime.datetime.now().weekday())
-    switch = {
-        '0': '\t\t\t恭喜吴一艇写日报！！！',
-        '1': '\t\t\t恭喜廖义权写日报！！！',
-        '2': '\t\t\t恭喜沈成写日报！！！',
-        '3': '\t\t\t恭喜郑小兰写日报！！！',
-        '4': '\t\t\t恭喜乌东宇写日报！！！',
-        '5': '\t\t\t再次恭喜乌东宇写日报！！！',
-        '6': '\t\t\t今天老子不上班！！！',
-    }
-    return switch[week]
 
 def getCurrentTime():
     currentTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
